91e303a840fd3b62df3d6525b6ce639d1f21067d
Alex <alex@pop-os.localdomain>
2022-01-08 12:27:09 -0800

    adding authentication middleware route for further api access

--- middleware/authorizedMiddleware.go
+++ middleware/authorizedMiddleware.go
@@ -1,1 +1,20 @@
 package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"tokens"
+)
+
+func VerifyAuthorization(c *fiber.Ctx) error {
+	cookie := c.Cookies("rest_cookie") // synchronize with authController - use property file
+
+	if _, err := tokens.VerifyJWT(cookie); err != nil {
+		c.Status(fiber.StatusUnauthorized)
+
+		return c.JSON(fiber.Map{
+			"response": "Unable to verify login",
+		})
+	}
+
+	return c.Next()
+}
