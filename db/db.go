1d4a5f05c6af2cb1fd94a7073f7f4e8794edfae9
Alex <alex@pop-os.localdomain>
2022-01-08 12:20:19 -0800

    adding database connection

--- db/db.go
+++ db/db.go
@@ -1,1 +1,23 @@
 package db
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"models"
+)
+
+var Instance *gorm.DB
+
+func Connect(path string) {
+	db, err := gorm.Open(mysql.Open(path), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// set the pointer instance to the connected database
+	Instance = db
+
+	//func (db *DB) AutoMigrate(dst ...interface{}) error
+	db.AutoMigrate(&models.Profile{})
+}
